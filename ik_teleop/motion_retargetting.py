ba6c7dd4327dc3a5d6c69ee5384295acb913b0fa
kopiotrek <kopiotrek@gmail.com>
2025-03-06 13:11:51 +0100

    scaling debugging

--- ik_teleop/motion_retargetting.py
+++ ik_teleop/motion_retargetting.py
@@ -256,6 +256,7 @@ class AllegroRetargetingOptimizer:
             'ring': 0.1527,
             'thumb': 0.1308, #0.0363 is real length of servo no.1
         }
+        scale_compensation = 1.1 # to make the allegro fingers extended for real
 
         # Calculate scaling factors for each finger based on their lengths
         finger_scales = {}
@@ -266,6 +267,7 @@ class AllegroRetargetingOptimizer:
             for i in range(len(finger_joints) - 1):
                 link_length = np.linalg.norm(finger_joints[i] - finger_joints[i + 1])
                 total_length += link_length
+            print(f"finger {finger} total_length {total_length}")
 
             # Calculate the scaling factor (robot length / XR length)
             scaling_factor = robot_finger_lengths[finger] / total_length
